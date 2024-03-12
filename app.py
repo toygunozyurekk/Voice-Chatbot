6f155dc87527d12150a014bef5b9228d67f62d42
toygunozyurekk <toygun.ozyurek@gmail.com>
2024-03-12 23:56:30 +0300

    Last version of backend

--- app.py
+++ app.py
@@ -3,15 +3,8 @@ from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 import os
 from flask_cors import CORS
-import whisper
 from tempfile import NamedTemporaryFile
-from langchain_community.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader, PyPDFLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
-from langchain_community.vectorstores import Pinecone
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain_community.chat_models import ChatOpenAI
-from langchain.chains.question_answering import load_qa_chain
+
 
 
 load_dotenv()
@@ -21,24 +14,6 @@ client = OpenAI(api_key=OPENAI_API_KEY)
 app = Flask(__name__)
 CORS(app)
 
-# Calling transcribe method with whisper library(If you want to use that it's just gonna transcribe to voice :) ) 
-@app.route('/transcribe', methods=['POST'])
-def transcribe_audio():
-    model=whisper.load_model("base")
-    if not request.files:
-        return jsonify({'error': "Audio file is required."}), 400
-
-    results = []
-
-    for filename, handle in request.files.items():
-        temp = NamedTemporaryFile()
-        handle.save(temp)
-        result = model.transcribe(temp.name)
-        results.append({
-            'filename': filename,
-            'transcript': result['text'],
-        })
-    return {'results': results}
 
 # Calling text to text model with openai
 @app.route('/get_response', methods=['POST'])
@@ -103,36 +78,6 @@ def get_openai_text_response(message):
     except Exception as e:
         return {'error': str(e)}
 
-
-
-
-
-@app.route('/askapdf', methods=['POST'])
-def handle_pdf_and_askapdf():
-    messages = []
-
-    for file_name, file in request.files.items():
-        with NamedTemporaryFile(suffix=".pdf", delete=False) as temp:
-            file.save(temp)
-            temp_path = temp.name  # Save the temp file path to use in PyPDFLoader
-
-        
-            # Assuming PyPDFLoader takes a file path and loads or processes the PDF file
-            loader = PyPDFLoader(file_path=temp_path)
-            data = loader.load()  # Load or process the PDF file
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-    texts = text_splitter.split_documents(data)
-    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-    vectorstore = Chroma.from_documents(texts, embeddings)
-    query = request.json.get('message', '')
-    docs = vectorstore.similarity_search(query)
-    llm = ChatOpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
-    chain = load_qa_chain(llm, chain_type="stuff")
-    query = request.json.get('message', '')
-    docs = vectorstore.similarity_search(query)
-    result = chain.run(input_documents=docs, question=query)
-    messages.append(result)
-    return messages
 
 if __name__ == '__main__':
     app.run(debug=True)
